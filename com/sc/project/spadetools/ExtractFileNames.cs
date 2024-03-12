281632085ffc11d3f4aa5d326e4cbf2676a4b873
JackSpade-AcerAspire3 <sethcortel12@gmail.com>
2024-03-13 01:30:10 +0800

    202401312-002 : 2024.03.13 - Added functions for the logic of ExtractFIleNames class and also created the entity class for the class

--- com/sc/project/spadetools/ExtractFileNames.cs
+++ com/sc/project/spadetools/ExtractFileNames.cs
@@ -1,29 +1,126 @@
 
-namespace com.sc.project.spadetools {
+namespace com.sc.project.spadetools 
+{
     
-    class ExtractFileNames {
+    class ExtractFileNames : ExtractFileNamesEntity
+    {
 
-        // Primary Variables
-        private List<string> Files { get; set; }
-        private List<string> FileTypes { get; set; }
-        private List<String> FailedDirectories { get; set; }
-        private File rootDirectory { get; set; }
+        // Properties
+        protected string RootDirectory
+        {
+            get
+            {
+                return rootDirectory.GetFullPath();
+            }
+            
+            set
+            {
+                // todo : create IsValidFile(string filepath) function
+                if (IsValidFile(value))
+                {
+                    rootDirectory = value;
+                }
+                else
+                {
+                    // todo : add throw error
+                }
+            }
+        }
 
-
-        // Supporting Variables
-        private bool isFullPath;
-        private bool isGroupByFileTypes;
-        private bool isSaveOutput;
-
+        protected string SaveDirectory {
+            get 
+            {
+                return saveDirectory;
+            }
+             
+            set
+            {
+                // todo : create IsValidFile(string filepath) function
+                if (IsValidFile(value))
+                {
+                    saveDirectory = value;
+                }
+                else
+                {
+                    saveDirectory = rootDirectory;
+                }
+            }
+        }
+        
         public ExtractFileNames(string[] arguments)
         {
-
+            rootDirectory = "";
+            saveDirectory = "";
+            files = new List<string>();
+            fileTypes = new List<string>();
+            excludedFileTypes = new List<string>();
+            failedDirectories = new List<string>();
+            isFullPath = false;
+            isGroupByFileTypes = false;
+            isSaveOutput = false;
         }
 
-        protected virtual SetArguments(string[] arguments)
+        protected virtual void SetArguments(string[] arguments)
         {
-            
+            int argumentCount = arguments.size();
+            string nextArgument = "";
+            for (int i = 0; i < argumentCount; i++)
+            {
+                // Coalesce (next argument, blank)
+                nextArgument = i < argumentCount ? arguments[i + 1] : "";
+
+                if (IsParameter(arguments[i]))
+                {
+                    SetProperty(arguments[i], nextArgument);
+                }
+            }
         }
+
+        protected virtual bool IsParameter(string argument)
+        {
+            return argument[0] == '-';
+        }
+
+        protected virtual void SetProperty(string argument, string nextArgument)
+        {
+            switch(argument)
+            {
+                case "-fullpath":
+                case "-fp":
+                    isFullPath = true;
+                    break;
+
+                case "-groupbyfiletype":
+                case "-gft":
+                    isGroupByFileTypes = true;
+                    break;
+
+                case "-exclude":
+                case "-e":
+                    AddToExcludedFileTypes(nextArgument);
+                    break;
+
+                case "-save":
+                case "-s":
+                    isSaveOutput = true;
+                    SaveDirectory = nextArgument;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private virtual void AddToExcludedFileTypes(string parameters)
+        {
+            string[] excludedParameters = String.Split(',', parameters);
+            foreach (string excludedParameter in excludedParameters)
+            {
+                ExcludedFileTypes.Add(excludedParameter);
+            }
+        }
+
+
 
     }
 
---
+++ com/sc/project/spadetoolsentity/ExtractFileNamesEntity.cs
@@ -0,0 +1,16 @@
+﻿
+namespace com.sc.project.spadetoolsentity
+{
+    protected class ExtractFileNamesEntity
+    {
+        private File rootDirectory;
+        private string saveDirectory;
+        private List<string> files;
+        private List<string> fileTypes;
+        private List<string> excludedFileTypes;
+        private List<string> failedDirectories;
+        private bool isFullPath;
+        private bool isGroupByFileTypes;
+        private bool isSaveOutput;
+    }
+}
